9644383458653d340eb8553bcf03c3afe560f58e
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-17 18:38:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
-  // your code here
+	let ans = ""
+	if(s.length == 0){
+		return ans
+	}
+	let str = s.trim()
+	for(let i of str){
+		if(i == ' '){
+			break
+		}
+		ans += i
+	}
+	return ans
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
